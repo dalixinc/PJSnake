f99f4453c1c602c26d1df6182eec49acea6dd400
Dale <dale.macdonald@closebrothers.com>
2024-08-01 21:42:40 +0100

    Added Vecto2 class to java so we can continue to mirror

--- PJSnake.java
+++ PJSnake.java
@@ -2,14 +2,21 @@ import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
 import javax.swing.event.*;
+import java.util.Random;
 
 public class PJSnake extends JFrame implements ActionListener{
 
     int cell_size = 40;
     int cell_number = 20;
+    Random random = new Random();
 
     public static void main(String[] args) {
         new PJSnake();
+        Vector2 v1 = new Vector2(7, 8);
+        Vector2 v2 = new Vector2(2, 3);
+        Vector2 sum = new Vector2(0, 0);
+        sum = sum.add(v1, v2);
+        System.out.println(sum.x + " , " + sum.y);
     }
 
     public PJSnake() {
@@ -38,13 +45,28 @@ public class PJSnake extends JFrame implements ActionListener{
         }
     }
 
-    class Apple {
-        int appleX = 5;
-        int appleY = 4;
+    class Apple {private
+        int appleX = random.nextInt(cell_number -1);
+        int appleY = random.nextInt(cell_number -1);
 
         public void drawApple(Graphics g) {
             g.setColor(new Color(126, 155, 114));
-            g.fillRect(appleX, appleY, cell_size, cell_size  );
+            g.fillRect(appleX * cell_size, appleY * cell_size, cell_size, cell_size  );
+        }
+    }
+
+    class Vector2 {
+
+         int x;
+         int y;
+
+         Vector2(int x, int y) {
+            this.x = x;
+            this.y = y;
+        }
+
+        Vector2 add (Vector2 v1, Vector2 v2) {
+            return new Vector2(v1.x + v2.x, v1.y + v2.y);
         }
     }
 }
--- PJSnake.py
+++ PJSnake.py
@@ -1,14 +1,14 @@
-import pygame, sys
+import pygame, sys, random
 from pygame.math import Vector2
 
 class APPLE:
     def __init__(self):
-        self.x = 5
-        self.y = 4
+        self.x = random.randint(0, cell_number -1)
+        self.y = random.randint(0, cell_number -1)
         self.pos = Vector2(self.x, self.y)
 
     def draw_apple(self):
-        apple_rect = pygame.Rect(self.pos.x, self.pos.y, cell_size, cell_size)
+        apple_rect = pygame.Rect(self.pos.x * cell_size, self.pos.y * cell_size, cell_size, cell_size)
         pygame.draw.rect(screen, (126, 166, 114),apple_rect)
 
 pygame.init()
